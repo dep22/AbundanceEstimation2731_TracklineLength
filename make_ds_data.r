36c49dba3f17abb5e2b7d4e3fff30bbd90a0d1ce
Dan Pendleton <dep22@cornell.edu>
2021-01-26 17:20:13 -0500

    This script is a frankenstein of ds_data.r to calculate distance of a survey record-by-record w/o thinking about Legno2.

--- make_ds_data.r
+++ make_ds_data.r
@@ -6,11 +6,12 @@ rm(list=ls())
 ## import data - !! data must be sorted YEAR > MONTH > DAY > FILEID > EVENTNO !!
 # alter this path for your computer
 #dat <- read.csv("/home/dan/Documents/WorkDocuments/Projects/MassCEC2731/Aerial survey data/BK Data Processed 2011 - 2018 ORIGINAL/NEAQ 2011-2019_PHSTRIP-dp1.csv")
-dat <- read.csv("C://Users//oobrien//Documents//R_Work_Directory//Distance//NEAQ 2011-2019_PHSTRIP-dp.csv")
+dat <- read.csv("/Volumes/vestigal/Documents/WorkDocuments/Projects/MassCEC2731/Aerial survey data/BK Data Processed 2011 - 2018 ORIGINAL/NEAQ 2011-2019_PHSTRIP-dp.csv")
+# dat <- read.csv("C://Users//oobrien//Documents//R_Work_Directory//Distance//NEAQ 2011-2019_PHSTRIP-dp.csv")
 
 ## select your season beginning and ending as "YEAR.MO"
-season.beg <- 2019.06
-season.end <- 2019.06
+season.beg <- 2019.01
+season.end <- 2019.12
 
 ## specify data directory and place where new files will be saved
 target_dir <- paste(getwd(), "/output/", sep="")
@@ -25,11 +26,11 @@ dat <- ds_data_prep(dat, season.beg, season.end, condensed)
 ## run ds_data.r
 # select species to include
 #spp = c("RIWH","FIWH","HUWH","SEWH")
-spp = c("RIWH")
+# spp = c("RIWH")
 #spp = c("RIWH","HUWH")
 #spp = c("RIWH","HUWH","FIWH","SEWH")
 
 #set phstrip to 0 or 1.
-phstrip = 1
-source('ds_data.r')
-ds_data(dat, target_dir, spp, phstrip)
+# phstrip = 1
+# source('ds_data_wLegno999.r')
+# ds_data(dat, target_dir, spp, phstrip)
